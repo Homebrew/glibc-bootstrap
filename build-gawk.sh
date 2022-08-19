9bb02e3ead491ef324f5abfc3ae7e572d5768319
danielnachun <daniel.nachun@gmail.com>
2022-08-19 01:36:32 -0700

    build-gawk.sh: add

---
+++ build-gawk.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -e
+source "$(dirname "${BASH_SOURCE[0]}")/utils.sh"
+
+VERSION=5.1.1
+SHA256SUM=d87629386e894bbea11a5e00515fc909dc9b7249529dad9e6a3a2c77085f7ea2
+
+# Build gawk
+wget --no-check-certificate https://ftp.gnu.org/gnu/gawk/gawk-$VERSION.tar.xz
+verify_checksum gawk-$VERSION.tar.xz $SHA256SUM
+
+tar --extract --file gawk-$VERSION.tar.xz
+cd gawk-$VERSION
+./configure --prefix="${PREFIX}" --disable-mpfr --without-libsigsegv
+make
+make install
+cd ..
+rm --recursive --force gawk-$VERSION
+
+package gawk ${VERSION}
