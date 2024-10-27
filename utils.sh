4e4c3ee473636ed5ae44355ea5fd378785b3cc26
Ruoyu Zhong <zhongruoyu@outlook.com>
2024-10-27 23:26:58 +0800

    utils.sh: fix incompatibility with CentOS 7 coreutils
    Fixes:
    
        tar: .: Cannot rmdir: Invalid argument
        tar: Exiting with failure status due to previous errors

--- utils.sh
+++ utils.sh
@@ -3,20 +3,20 @@
 set -e
 
 verify_checksum() {
-  FILE=$1
-  EXPECTED_CHECKSUM=$2
-  FILE_CHECKSUM=$(sha256sum ${FILE} | cut -d ' ' -f 1)
+  FILE="$1"
+  EXPECTED_CHECKSUM="$2"
+  FILE_CHECKSUM="$(sha256sum "${FILE}" | cut -d ' ' -f 1)"
 
-  if [[ $FILE_CHECKSUM != $EXPECTED_CHECKSUM ]]; then
+  if [[ "$FILE_CHECKSUM" != "$EXPECTED_CHECKSUM" ]]; then
     echo "Checksum mismatch!"
     return 1
   fi
 }
 
 package() {
-  PKGNAME=$1
-  VERSION=$2
+  PKGNAME="$1"
+  VERSION="$2"
 
-  cd $PREFIX
-  tar --remove-files --create --gzip --file $PKGDIR/bootstrap-$PKGNAME-$VERSION.tar.gz .
+  tar -C "$PREFIX" -czvf "$PKGDIR/bootstrap-$PKGNAME-$VERSION.tar.gz" .
+  find "$PREFIX" -mindepth 1 -delete
 }
