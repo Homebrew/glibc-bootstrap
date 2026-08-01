5a2dedfb14d0c250b524448b68c78daa824aa7ce
Michael Cho <michael@michaelcho.dev>
2026-08-01 13:32:42 -0400

    build-python3.sh: bump to 3.11.15

--- build-python3.sh
+++ build-python3.sh
@@ -3,8 +3,8 @@
 set -e
 source "$(dirname "${BASH_SOURCE[0]}")/utils.sh"
 
-VERSION=3.11.10
-SHA256SUM=07a4356e912900e61a15cb0949a06c4a05012e213ecd6b4e84d0f67aabbee372
+VERSION=3.11.15
+SHA256SUM=272179ddd9a2e41a0fc8e42e33dfbdca0b3711aa5abf372d3f2d51543d09b625
 
 # Build Python 3
 wget --no-check-certificate https://www.python.org/ftp/python/$VERSION/Python-$VERSION.tar.xz
